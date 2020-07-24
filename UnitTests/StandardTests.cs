9e818472847d37ed855b82e515999d143d25827f
Xeek <xeek@xeekworx.com>
2020-07-23 23:55:18 -0500

    Added comments so that future Xeek pretends to know what he's doing until he finally knows what he's doing. Also, lets' get that .github folder into the project.

--- UnitTests/StandardTests.cs
+++ UnitTests/StandardTests.cs
@@ -12,21 +12,59 @@ namespace SDL_HelpBot.UnitTests
     [TestFixture]
     public class StandardTests
     {
+        private const string DiscordTokenEnvName = "SDL_HELPBOT_DISCORDTOKEN";
+        private string DiscordToken { get; set; }
+
         [SetUp]
         public void SetUp()
         {
-
+            /*
+             * The .github workflow file should contain the lines in between the following hyphen 
+             * separators:
+             * ------------------------------------------------------------------------------------
+             * 
+             *    - name: Test
+             *      env:
+             *          SDL_HELPBOT_DISCORDTOKEN: ${{ secrets.SDL_HELPBOT_DISCORDTOKEN }}
+             *      run: dotnet test --no-restore --verbosity normal
+             *      
+             * ------------------------------------------------------------------------------------
+             * If this is being executed on a system not from GitHub Actions, then you will need
+             * to create an environment variable named SDL_HELPBOT_DISCORDTOKEN with your key.
+             */
+            DiscordToken = Environment.GetEnvironmentVariable(DiscordTokenEnvName);
         }
 
         [Test]
-        public void CanIGetTheDiscordToken()
-        {
-            string token;
-            Assert.IsFalse(string.IsNullOrEmpty(token = Environment.GetEnvironmentVariable("SDL_HELPBOT_DISCORDTOKEN")));
-            Console.WriteLine($"Found Discord Token: {token}");
+        public void ValidateDiscordToken()
+        {   /* 
+             * If this continues to fail repeatedly after you've setup the environment variable 
+             * there are two possibilities:
+             * 
+             *  1.  You need to restart Visual Studio, the shell, power shell, or the command 
+             *      prompt that you're running these tests from so that the environment variables 
+             *      get refreshed.
+             *
+             *  2.  You haven't created the GitHub Secret for your repository or the GitHub 
+             *      Workflow file in the .github folder does not have the env section for your 
+             *      GitHub Action.
+             */
+
+            Assert.IsNotNull(
+                DiscordToken,
+                $"The discord token doesn't exist, it's likely you forgot to create an environment variable named {DiscordTokenEnvName}."
+            );
+
+            Assert.IsNotEmpty(
+                DiscordToken,
+                $"The discord token exists, but it's empty, you probably need to set the environment variable named {DiscordTokenEnvName}" + 
+                " to your token by finding it in the bot category of your Discord application." +
+                " Go here: https://discord.com/developers/applications"
+            );
         }
 
         [Test]
+        [TestOf(typeof(DiscordSocketClient))]
         public void LoginToDiscord()
         {
             using var services = new ServiceCollection()
@@ -37,13 +75,9 @@ namespace SDL_HelpBot.UnitTests
                 .AddSingleton<PictureService>()
                 .BuildServiceProvider();
 
-            //SDL_HELPBOT_DISCORDTOKEN: ${{ secrets.SDL_HELPBOT_DISCORDTOKEN }}
-
-            services
+            Assert.DoesNotThrowAsync(() => services
                 .GetRequiredService<DiscordSocketClient>()
-                .LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("SDL_HELPBOT_DISCORDTOKEN"))
-                .GetAwaiter()
-                .GetResult();
+                .LoginAsync(TokenType.Bot, DiscordToken), "Failed to login to Discord, is your token correct?");
         }
     }
 }
