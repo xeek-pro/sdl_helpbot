fedf604efd46deb91af090930727e1e6cdc81561
John Tullos <xeek@xeekworx.com>
2020-07-23 20:46:19 -0500

    Added initial unit testing

---
+++ UnitTests/StandardTests.cs
@@ -0,0 +1,39 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using NUnit.Framework;
+using SDL_HelpBot.Services;
+using System;
+using System.Net.Http;
+
+namespace SDL_HelpBot.UnitTests
+{
+    [TestFixture]
+    public class StandardTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public async void LoginToDiscord()
+        {
+            using var services = new ServiceCollection()
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandlingService>()
+                .AddSingleton<HttpClient>()
+                .AddSingleton<PictureService>()
+                .BuildServiceProvider();
+
+            //SDL_HELPBOT_DISCORDTOKEN: ${{ secrets.SDL_HELPBOT_DISCORDTOKEN }}
+
+            var client = services.GetRequiredService<DiscordSocketClient>();
+            await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("SDL_HELPBOT_DISCORDTOKEN"));
+        }
+    }
+}
